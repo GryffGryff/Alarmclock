eeb3477a3a386285b13cccb1347883ec3be5077b
Nisha <elenanisha@gmail.com>
2018-07-22 21:46:57 +0000

    Added LED support and debugging button

--- Alarmclock/src/alarmclock/Alarmclock.java
+++ Alarmclock/src/alarmclock/Alarmclock.java
@@ -6,6 +6,8 @@
 package alarmclock;
 
 import java.time.LocalTime;
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.*;
 
 /**
  *
@@ -30,6 +32,48 @@ public class Alarmclock {
         //Alarm alarm = new Alarm();
         //alarm.setAlarmTime(13, 47, 0);
         //alarm.checkAlarm();
+        
+
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // provision gpio pin #01 as an output pin and turn on
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+
+        // set shutdown state for this pin
+        pin.setShutdownOptions(true, PinState.LOW);
+
+        // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+
+        // set shutdown state for this input pin
+        myButton.setShutdownOptions(true);
+
+        // create and register gpio pin listener
+        myButton.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+            }
+
+        });
+
+        System.out.println(" ... complete the GPIO #02 circuit and see the listener feedback here in the console.");
+
+        // keep program running until user aborts (CTRL-C)
+        while(true) {
+            try{
+                Thread.sleep(500);
+            } catch (java.lang.InterruptedException ie) {
+            }
+        }
+
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        //gpio.shutdown();
+
+        //System.out.println("Exiting ControlGpioExample");
     }
     
 }
