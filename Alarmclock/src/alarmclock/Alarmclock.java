f3b46be79456e488c73256636d4dab926ec14297
Gryffin <elenanisha@gmail.com>
2018-07-24 20:13:35 -0700

    made new class called UserInput to acess input from buttons

--- Alarmclock/src/alarmclock/Alarmclock.java
+++ Alarmclock/src/alarmclock/Alarmclock.java
@@ -6,8 +6,6 @@
 package alarmclock;
 
 import java.time.LocalTime;
-import com.pi4j.io.gpio.*;
-import com.pi4j.io.gpio.event.*;
 
 /**
  *
@@ -30,74 +28,9 @@ public class Alarmclock {
         //System.out.println(clock.getTime());
         //clock.update();
 
-        //Alarm alarm = new Alarm();
-        //alarm.setAlarmTime(13, 47, 0);
-        //alarm.checkAlarm();
-        
-
-        // create gpio controller
-        final GpioController gpio = GpioFactory.getInstance();
-
-        // provision gpio pin #01 as an output pin and turn on
-        // final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.LOW);
-
-        // set shutdown state for this pin
-        //pin.setShutdownOptions(true, PinState.LOW);
-
-        // provision gpio pin #01 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput select = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, PinPullResistance.PULL_DOWN);
-        // provision gpio pin #04 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput up = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04, PinPullResistance.PULL_DOWN);
-        // provision gpio pin #05 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput down = gpio.provisionDigitalInputPin(RaspiPin.GPIO_05, PinPullResistance.PULL_DOWN);
-        
-        // set shutdown state for this input pin
-        select.setShutdownOptions(true);
-        // set shutdown state for this input pin
-        up.setShutdownOptions(true);
-        // set shutdown state for this input pin
-        down.setShutdownOptions(true);
-
-        // create and register gpio pin listener
-        select.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                // display pin state on console
-                System.out.println(" --> SELECT BUTTON STATE CHANGE: " + event.getPin() + " = " + event.getState());
-            }
-        });
-        up.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                // display pin state on console
-                System.out.println(" --> UP BUTTON STATE CHANGE: " + event.getPin() + " = " + event.getState());
-            }
-        });
-        down.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                // display pin state on console
-                System.out.println(" --> DOWN BUTTON STATE CHANGE: " + event.getPin() + " = " + event.getState());
-            }
-        });
-
-        System.out.println(" ... complete the GPIO #02 circuit and see the listener feedback here in the console.");
-
-        // keep program running until user aborts (CTRL-C)
-        while(true) {
-            try{
-                Thread.sleep(500);
-            } catch (java.lang.InterruptedException ie) {
-            }
-        }
-
-        // stop all GPIO activity/threads by shutting down the GPIO controller
-        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        //gpio.shutdown();
-
-        //System.out.println("Exiting ControlGpioExample");
-        //Alarm alarm = new Alarm();
-        //alarm.setAlarmTime(11, 28, 0, Alarm.DADDY_ALARM);
-        //alarm.checkAlarm();
+        Alarm alarm = new Alarm();
+        alarm.setAlarmTime(13, 47, 0, Alarm.DADDY_ALARM);
+        alarm.setAlarmTime(12, 54, 0, Alarm.MOMMY_ALARM)
+        alarm.checkAlarm();
     }
 }
